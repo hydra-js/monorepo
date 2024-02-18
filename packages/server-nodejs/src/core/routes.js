03d9389b2424fe6cbe55026cd6c94547bd3a8944
thinkholic <mailtokasun@gmail.com>
2024-02-18 21:22:18 +0100

    add dummy routes

--- packages/server-nodejs/src/core/routes.js
+++ packages/server-nodejs/src/core/routes.js
@@ -3,19 +3,67 @@ const express = require('express');
 const consoleRouter = express.Router();
 const defaultRouter = express.Router();
 
+// Console API Routes
+
 consoleRouter.get('/', (_, res) => {
   res.status(200).json({
     code: 200,
-    message: 'This endpoint will return OpenAPI Specification in JSON',
+    message: {
+      info: {
+        title: 'Hydra Console',
+        version: '1.0.0-alpha.0',
+        description: 'This endpoint will return OpenAPI Specification in JSON',
+      },
+      paths: {},
+    },
   });
 });
+
+// users
+consoleRouter.get('/users', (_, res) => res.status(200).json({}));
+consoleRouter.get('/users/:id', (_, res) => res.status(200).json({}));
+consoleRouter.post('/users', (_, res) => res.status(200).json({}));
+consoleRouter.patch('/users/:id', (_, res) => res.status(200).json({}));
+consoleRouter.delete('/users/:id', (_, res) => res.status(200).json({}));
+
+// namespace
+consoleRouter.get('/namespace', (_, res) => res.status(200).json({}));
+consoleRouter.get('/namespace/:name', (_, res) => res.status(200).json({}));
+consoleRouter.post('/namespace', (_, res) => res.status(200).json({}));
+consoleRouter.put('/namespace/:name', (_, res) => res.status(200).json({}));
+consoleRouter.patch('/namespace/:name', (_, res) => res.status(200).json({}));
+consoleRouter.delete('/namespace/:name', (_, res) => res.status(200).json({}));
+
+// resouce
+consoleRouter.get('/resource', (_, res) => res.status(200).json({}));
+consoleRouter.get('/resource/:name', (_, res) => res.status(200).json({}));
+consoleRouter.post('/resource', (_, res) => res.status(200).json({}));
+consoleRouter.put('/resource/:name', (_, res) => res.status(200).json({}));
+consoleRouter.patch('/resource/:name', (_, res) => res.status(200).json({}));
+consoleRouter.delete('/resource/:name', (_, res) => res.status(200).json({}));
+
+// Default Server API Routes
 
 defaultRouter.get('/', (_, res) => {
   res.status(200).json({
     code: 200,
-    message:
-      'This endpoint will return OpenAPI Specification in JSON (defaultRouter)',
+    message: {
+      info: {
+        title: 'Hydra Default Server',
+        version: '1.0.0-alpha.0',
+        description:
+          'This endpoint will return OpenAPI Specification in JSON (Default Server)',
+      },
+      paths: {},
+    },
   });
 });
+
+defaultRouter.get('/:resource/:id', (_, res) => res.status(200).json({}));
+defaultRouter.get('/:resource/', (_, res) => res.status(200).json({}));
+defaultRouter.post('/:resource', (_, res) => res.status(200).json({}));
+defaultRouter.put('/:resource/:id', (_, res) => res.status(200).json({}));
+defaultRouter.patch('/:resource/:id', (_, res) => res.status(200).json({}));
+defaultRouter.delete('/:resource/:id', (_, res) => res.status(200).json({}));
 
 module.exports = { consoleRouter, defaultRouter };
