aba9743280e0cb78b96d772f63fc85cd32d20a6d
thinkholic <mailtokasun@gmail.com>
2024-02-20 00:33:22 +0100

    add Unauthorized component

--- packages/console/src/layouts/Root.tsx
+++ packages/console/src/layouts/Root.tsx
@@ -1,7 +1,20 @@
+import Unauthorized from 'src/screens/Unauthorized';
+
 type Props = {
   children: JSX.Element | JSX.Element[];
+  enableAuth?: boolean;
 };
 
-export default function RootLayout({ children }: Props) {
+function getAuth() {
+  // @todo: get auth object
+  return {};
+}
+
+export default function RootLayout({ children, enableAuth = false }: Props) {
+  if (enableAuth) {
+    const auth = getAuth();
+    if (!auth) return <Unauthorized />;
+  }
+
   return <>{children}</>;
 }
---
+++ packages/console/src/screens/Unauthorized.tsx
@@ -0,0 +1,7 @@
+export default function Unauthorized() {
+  return (
+    <div>
+      <p>Unauthorized</p>
+    </div>
+  );
+}
